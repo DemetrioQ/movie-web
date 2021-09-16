f79b93b790a7daadf7dd3126c6c1512ef5dc081c
Demetrio Quiñones <demetriorqe@gmail.com>
2021-09-15 23:25:27 -0400

    updated

--- src/views/PageWrapper.js
+++ src/views/PageWrapper.js
@@ -121,103 +121,9 @@ function PageWrapper(props) {
                   </a>
                 </li>
                 <li className='dropdown first'>
-                  <a lassName='btn btn-default dropdown-toggle lv1' data-toggle='dropdown' data-hover='dropdown'>
-                    movies<i className='fa fa-angle-down' aria-hidden='true'></i>
-                  </a>
-                  <ul className='dropdown-menu level1'>
-                    <li className='dropdown'>
-                      <a href='#' className='dropdown-toggle' data-toggle='dropdown'>
-                        Movie grid<i className='ion-ios-arrow-forward'></i>
-                      </a>
-                      <ul className='dropdown-menu level2'>
-                        <li>
-                          <a href='moviegrid.html'>Movie grid</a>
-                        </li>
-                        <li>
-                          <a href='moviegridfw.html'>movie grid full width</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href='movielist.html'>Movie list</a>
-                    </li>
-                    <li>
-                      <a href='moviesingle.html'>Movie single</a>
-                    </li>
-                    <li className='it-last'>
-                      <a href='seriessingle.html'>Series single</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className='dropdown first'>
-                  <a className='btn btn-default dropdown-toggle lv1' data-toggle='dropdown' data-hover='dropdown'>
-                    celebrities <i className='fa fa-angle-down' aria-hidden='true'></i>
-                  </a>
-                  <ul className='dropdown-menu level1'>
-                    <li>
-                      <a href='celebritygrid01.html'>celebrity grid 01</a>
-                    </li>
-                    <li>
-                      <a href='celebritygrid02.html'>celebrity grid 02 </a>
-                    </li>
-                    <li>
-                      <a href='celebritylist.html'>celebrity list</a>
-                    </li>
-                    <li className='it-last'>
-                      <a href='celebritysingle.html'>celebrity single</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className='dropdown first'>
                   <a href='/blog' className='btn btn-default dropdown-toggle lv1'>
                     BLOG
                   </a>
-                </li>
-                <li className='dropdown first'>
-                  <a className='btn btn-default dropdown-toggle lv1' data-toggle='dropdown' data-hover='dropdown'>
-                    community <i className='fa fa-angle-down' aria-hidden='true'></i>
-                  </a>
-                  <ul className='dropdown-menu level1'>
-                    <li>
-                      <a href='userfavoritegrid.html'>user favorite grid</a>
-                    </li>
-                    <li>
-                      <a href='userfavoritelist.html'>user favorite list</a>
-                    </li>
-                    <li>
-                      <a href='userprofile.html'>user profile</a>
-                    </li>
-                    <li className='it-last'>
-                      <a href='userrate.html'>user rate</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <ul className='nav navbar-nav flex-child-menu menu-right'>
-                <li className='dropdown first'>
-                  <a className='btn btn-default dropdown-toggle lv1' data-toggle='dropdown' data-hover='dropdown'>
-                    pages <i className='fa fa-angle-down' aria-hidden='true'></i>
-                  </a>
-                  <ul className='dropdown-menu level1'>
-                    <li>
-                      <a href='landing.html'>Landing</a>
-                    </li>
-                    <li>
-                      <a href='404.html'>404 Page</a>
-                    </li>
-                    <li className='it-last'>
-                      <a href='comingsoon.html'>Coming soon</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href='#'>Help</a>
-                </li>
-                <li className='loginLink'>
-                  <a href='#'>LOG In</a>
-                </li>
-                <li className='btn signupLink'>
-                  <a href='#'>sign up</a>
                 </li>
               </ul>
             </div>
@@ -276,9 +182,8 @@ function PageWrapper(props) {
                   <i className='ion-grid'></i>
                 </a>
               </div>
-              
+
               {props.children}
-              
             </div>
             <div className='col-md-4 col-sm-12 col-xs-12'>
               <div className='sidebar'>
